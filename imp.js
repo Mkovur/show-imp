26bd7995034a1f224143bed8a9d0dd3b91f7f9fa
Mkovur <144019060+Mkovur@users.noreply.github.com>
2024-08-27 19:52:53 +0100

    Add files via upload

---
+++ imp.js
@@ -0,0 +1,32 @@
+$(document).ready(function( ){
+//open menu
+    $(".right-menu").click(function () {
+          $(".open-menu").css({                      
+    display:"inherit",
+                   })
+     $(".open-menu").fadeIn("slow")  ;
+
+                             })                 
+     $("#angle-up").click(function () {
+          $(".open-menu").fadeOut("slow")
+                            })     
+    //remove menu
+        $(".right-menu").click(function () {
+          $("menu").css({                      
+   display:"none",
+      })
+    $("menu").fadeOut("slow")  ;
+                      
+                          })                              
+                            
+     //return menu
+     $("#angle-up").click(function () {
+        $("menu").fadeIn("slow")
+                          }) 
+                          //arrow hover
+                      
+                    
+                                            
+})
+
+                                        
